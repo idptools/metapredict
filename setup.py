49fdd47303823d3054a369a9ffcbd1411fb5c08b
Ryan <remenecker@wustl.edu>
2022-07-08 12:08:58 -0500

    fixed metapredict-name function

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ setup(
             'protfasta',
             'scipy',
             'urllib3',
-            'alphaPredict==1.0'],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+            'alphaPredict==1.0',
+            'getSequence'],              # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
