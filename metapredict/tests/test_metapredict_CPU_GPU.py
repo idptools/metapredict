9bcccae38a3370b007fc803e449d1d229d862c8f
Ryan <remenecker@wustl.edu>
2024-04-16 16:24:34 -0500

    Added test for CPU and GPU functionality

--- metapredict/tests/test_metapredict_CPU_GPU.py
+++ metapredict/tests/test_metapredict_CPU_GPU.py
@@ -1,6 +1,10 @@
 '''
-function to make sure that metapredict can use the GPU and the CPU
+Testing to make sure that metapredict can use the GPU and the CPU
 for predicting pLDDT and disorder for all current versions of metapredict.
+
+It's a really simple test that tries out various permutations of disorder
+prediction and pLDDT prediction on CPU and GPU. It also makes sure results
+on CPU match those from GPU. 
 '''
 
 import metapredict as meta
@@ -16,17 +20,222 @@ onehundred_seqs = "{}/input_data/test_seqs_100.fasta".format(current_filepath)
 # read in sequences 
 sequences = protfasta.read_fasta(onehundred_seqs, invalid_sequence_action='convert')
 
-# test all versions of metapredict
-for version in metapredict_networks:
-    print(f'Running metapredict version {version} on CPU\n')
-    meta.predict_disorder(sequences, version=version, device='cpu')
-    print(f'Running metapredict version {version} on GPU')
-    meta.predict_disorder(sequences, version=version, device='cuda')
+# tests below
 
-for version in pplddt_networks:
-    print(f'Running pLDDT prediction version {version} on CPU\n')
-    meta.predict_pLDDT(sequences, pLDDT_version=version, device='cpu')
-    print(f'Running pLDDT prediction version {version} on GPU\n')
-    meta.predict_pLDDT(sequences, pLDDT_version=version, device='cuda')
+def test_disorder_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
 
-print('finished!')
+def test_disorder_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
+
+def test_disorder_v3_cpu(sequences=sequences):
+    version='v3'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
+
+def test_disorder_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
+
+def test_disorder_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
+
+def test_disorder_v3_gpu(sequences=sequences):
+    version='v3'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n')
+    meta.predict_disorder(sequences, version=version, device=device)
+
+def test_pLDDT_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device)
+
+def test_pLDDT_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device)
+
+def test_pLDDT_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device)
+
+def test_pLDDT_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device)
+
+def test_force_disable_batch_disorder_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no batch prediction\n')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_disorder_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no batch prediction\n')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_disorder_v3_cpu(sequences=sequences):
+    version='v3'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no batch prediction\n')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_disorder_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n, no batch prediction')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_disorder_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n, no batch prediction')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_disorder_v3_gpu(sequences=sequences):
+    version='v3'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}\n, no batch prediction')
+    meta.predict_disorder(sequences, version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_pLDDT_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}, no batch prediction\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_pLDDT_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}, no batch prediction\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_pLDDT_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}, no batch prediction\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, force_disable_batch=True)
+
+def test_force_disable_batch_pLDDT_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}, no batch prediction\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, force_disable_batch=True)
+
+
+def test_disable_pack_n_pad_disorder_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_disorder_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_disorder_v3_cpu(sequences=sequences):
+    version='v3'
+    device='cpu'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_disorder_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_disorder_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_disorder_v3_gpu(sequences=sequences):
+    version='v3'
+    device='cuda'
+    print(f'Running metapredict version {version} on {device}, no pack-n-pad\n')
+    meta.predict_disorder(sequences, version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_pLDDT_v1_cpu(sequences=sequences):
+    version='v1'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}, no pack-n-pad\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_pLDDT_v2_cpu(sequences=sequences):
+    version='v2'
+    device='cpu'
+    print(f'Running pLDDT prediction version {version} on {device}, no pack-n-pad\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_pLDDT_v1_gpu(sequences=sequences):
+    version='v1'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}, no pack-n-pad\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, disable_pack_n_pad=True)
+
+def test_disable_pack_n_pad_pLDDT_v2_gpu(sequences=sequences):
+    version='v2'
+    device='cuda'
+    print(f'Running pLDDT prediction version {version} on {device}, no pack-n-pad\n')
+    meta.predict_pLDDT(sequences, pLDDT_version=version, device=device, disable_pack_n_pad=True)
+
+
+def test_disorder_v1_cpu_vs_gpu(sequences=sequences):
+    version='v1'
+    print(f'Running metapredict version {version}, comparing CPU vs GPU scores.\n')
+    cpu_scores=meta.predict_disorder(sequences, version=version, device='cpu', round_values=False)
+    gpu_scores=meta.predict_disorder(sequences, version=version, device='cuda', round_values=False)
+    assert(np.round(cpu_scores, 3)==np.round(gpu_scores, 3)).all()
+
+def test_disorder_v2_cpu_vs_gpu(sequences=sequences):
+    version='v2'
+    print(f'Running metapredict version {version}, comparing CPU vs GPU scores.\n')
+    cpu_scores=meta.predict_disorder(sequences, version=version, device='cpu', round_values=False)
+    gpu_scores=meta.predict_disorder(sequences, version=version, device='cuda', round_values=False)
+    assert(np.round(cpu_scores, 3)==np.round(gpu_scores, 3)).all()
+
+def test_disorder_v3_cpu_vs_gpu(sequences=sequences):
+    version='v3'
+    print(f'Running metapredict version {version}, comparing CPU vs GPU scores.\n')
+    cpu_scores=meta.predict_disorder(sequences, version=version, device='cpu', round_values=False)
+    gpu_scores=meta.predict_disorder(sequences, version=version, device='cuda', round_values=False)
+    assert(np.round(cpu_scores, 3)==np.round(gpu_scores, 3)).all()
+
+def test_pLDDT_v1_cpu_vs_gpu(sequences=sequences):
+    version='v1'
+    print(f'Running pLDDT prediction version {version}, comparing CPU vs GPU scores.\n')
+    cpu_scores=meta.predict_pLDDT(sequences, pLDDT_version=version, device='cpu', round_values=False)
+    gpu_scores=meta.predict_pLDDT(sequences, pLDDT_version=version, device='cuda', round_values=False)
+    assert(np.round(cpu_scores, 3)==np.round(gpu_scores, 3)).all()
+
+def test_pLDDT_v2_cpu_vs_gpu(sequences=sequences):
+    version='v2'
+    print(f'Running pLDDT prediction version {version}, comparing CPU vs GPU scores.\n')
+    cpu_scores=meta.predict_pLDDT(sequences, pLDDT_version=version, device='cpu', round_values=False)
+    gpu_scores=meta.predict_pLDDT(sequences, pLDDT_version=version, device='cuda', round_values=False)
+    assert(np.round(cpu_scores, 3)==np.round(gpu_scores, 3)).all()
+
